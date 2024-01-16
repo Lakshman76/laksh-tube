264b6e81f7d30014ec16f3a7b837506d91a864a9
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-01-16 15:38:27 +0530

    Main body container

---
+++ src/components/MainContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import VideoContainer from './VideoContainer'
+import ButtonList from './ButtonList'
+
+const MainContainer = () => {
+  return (
+    <div className='mt-20'>
+        <ButtonList />
+        <VideoContainer />
+    </div>
+  )
+}
+
+export default MainContainer
