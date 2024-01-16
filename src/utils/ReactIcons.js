671f6933cebc4876679cbf3f8e56e77812e1f82c
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-01-16 16:26:22 +0530

    React icons for app

---
+++ src/utils/ReactIcons.js
@@ -0,0 +1,11 @@
+import { RxHamburgerMenu } from "react-icons/rx";
+import { PiMagnifyingGlass } from "react-icons/pi";
+import { MdNotificationsNone } from "react-icons/md";
+import { FaRegCircleUser } from "react-icons/fa6";
+
+export {
+    RxHamburgerMenu,
+    PiMagnifyingGlass,
+    MdNotificationsNone,
+    FaRegCircleUser,
+}
