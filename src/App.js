d8cd56d5252b1ae9240662233df481c393fb6a24
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-01-26 15:29:53 +0530

    provide route for each component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,32 @@ import "./App.css";
 import Body from "./components/Body";
 import Head from "./components/Head";
 import store from "./utils/store";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import MainContainer from "./components/MainContainer";
+import WatchPage from "./components/WatchPage";
 
 function App() {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Body />,
+      children: [
+        {
+          path: "/",
+          element: <MainContainer />,
+        },
+        {
+          path: "watch",
+          element: <WatchPage />,
+        },
+      ],
+    },
+  ]);
   return (
     <Provider store={store}>
       <div className="App">
         <Head />
-        <Body />
+        <RouterProvider router={appRouter} />
       </div>
     </Provider>
   );
