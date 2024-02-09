232d08a0f9c8a533ede718e06ef9d1d58fc22e43
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-02-09 15:10:50 +0530

    add some more react icons

--- src/utils/ReactIcons.js
+++ src/utils/ReactIcons.js
@@ -1,11 +1,17 @@
 import { RxHamburgerMenu } from "react-icons/rx";
-import { PiMagnifyingGlass } from "react-icons/pi";
+import { PiMagnifyingGlass, PiShareFatLight } from "react-icons/pi";
 import { MdNotificationsNone } from "react-icons/md";
 import { FaRegCircleUser } from "react-icons/fa6";
+import { BiLike, BiDislike } from "react-icons/bi";
+import { TfiDownload } from "react-icons/tfi";
 
 export {
-    RxHamburgerMenu,
-    PiMagnifyingGlass,
-    MdNotificationsNone,
-    FaRegCircleUser,
-}
+  RxHamburgerMenu,
+  PiMagnifyingGlass,
+  MdNotificationsNone,
+  FaRegCircleUser,
+  BiLike,
+  BiDislike,
+  PiShareFatLight,
+  TfiDownload,
+};
