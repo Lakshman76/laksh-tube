ee73e9aeb7e517a9530caad287fdefd1369fbd9f
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-02-12 16:13:19 +0530

    make an advance search bar

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../assets/images/LakshTube-logo.png";
 import {
   RxHamburgerMenu,
@@ -8,11 +8,31 @@ import {
 } from "../utils/ReactIcons";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../utils/sidenavSlice";
+import { YOUTUBE_SEARCH_API } from "../utils/constant";
 
 const Head = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+
   const dispatch = useDispatch();
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => getSearchSuggestion(), 200);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSuggestion = async () => {
+    // console.log(searchQuery);
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const jsonData = await data.json();
+    // console.log(jsonData);
+    setSuggestions(jsonData);
   };
 
   return (
@@ -26,15 +46,33 @@ const Head = () => {
           <img className="h-10" src={logo} alt="youtube-logo" />
         </a>
       </div>
-      <div className="flex justify-center items-center col-span-8">
-        <input
-          className="w-1/2 px-6 py-2 border border-gray-400 rounded-l-full outline-none"
-          type="text"
-          placeholder="Search"
-        />
-        <button className="p-2 border border-gray-400 rounded-r-full bg-gray-200">
-          <PiMagnifyingGlass className="w-8 h-6" />
-        </button>
+      <div className="flex flex-col col-span-9">
+        <div className="w-full flex justify-center items-center">
+          <input
+            className="w-1/2 px-8 py-2 border border-gray-400 rounded-l-full outline-none"
+            type="text"
+            placeholder="Search"
+            value={searchQuery}
+            onChange={(e) => {
+              setSearchQuery(e.target.value);
+            }}
+          />
+          <button className="p-2 border border-gray-400 rounded-r-full bg-gray-200">
+            <PiMagnifyingGlass className="w-8 h-6" />
+          </button>
+        </div>
+        <div className="fixed mt-12 ml-[17rem] p-2 w-[38rem] bg-white border border-gray-100 shadow-lg rounded-lg ">
+          <ul className="max-h-96 overflow-y-scroll">
+            {suggestions.map((suggestion) => {
+              // console.log(suggestion.show.name);
+              return (
+                <li className="flex gap-2 items-center p-2 hover:bg-slate-200">
+                  <PiMagnifyingGlass /> {suggestion.show.name}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
       <div className="flex justify-center items-center gap-4 col-span-2">
         <MdNotificationsNone className="w-8 h-8" />
