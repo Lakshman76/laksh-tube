ee3ff81fe3cd847adb8519d2074ae78555e5aa8c
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-02-23 15:49:44 +0530

    Resolve erroe and customize code

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -17,34 +17,12 @@ const ButtonList = () => {
     "Statitics",
     "Politics",
     "valentine",
-    "Stock market",
-    "Statitics",
-    "Politics",
-    "valentine",
-    "Stock market",
-    "Statitics",
-    "Politics",
-    "valentine",    
   ];
   return (
     <div className="flex w-[90vw] overflow-x-scroll ">
       {btnList.map((list) => (
-        <Button name={list} key={list}/>
+        <Button name={list} key={list} />
       ))}
-      {/* <Button name='All'/>
-      <Button name='Music'/>
-      <Button name='Computer programming'/>
-      <Button name='Live'/>
-      <Button name='Body building'/>
-      <Button name='Thoughts'/>
-      <Button name='News'/>
-      <Button name='Recently uploaded'/>
-      <Button name='Meditation music'/>
-      <Button name='Sales'/>
-      <Button name='Stock market'/>
-      <Button name='Statitics'/>
-      <Button name='Politics'/>
-      <Button name='Mantras'/> */}
     </div>
   );
 };
