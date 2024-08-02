99534e09d08f14582420c8d9971dce3d576ce2a5
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-08-02 16:50:56 +0530

    implement and add chat slice

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import sidenavSlice from "./sidenavSlice";
 import searchSlice from "./searchSlice";
+import chatSlice from "./chatSlice";
 
 const store = configureStore({
-    reducer: {
-        sidenav: sidenavSlice,
-        search : searchSlice,
-    },
-})
+  reducer: {
+    sidenav: sidenavSlice,
+    search: searchSlice,
+    chat: chatSlice,
+  },
+});
 
 export default store;
