465d3536e57a0226c0891db6dc5b215d855f3195
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-08-19 15:46:22 +0530

    make buttons searchable

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import getVideoBySearch from "../utils/getVideoBySearch";
+import { useDispatch } from "react-redux";
 
-const Button = ({name}) => {
+const Button = ({ name }) => {
+  const dispatch = useDispatch();
   return (
     <div>
-        <button className='px-4 py-1 bg-gray-300 m-2 rounded-lg'>{name}</button>
+      <Link to={`/search?q=${name}`}>
+        <button
+          onClick={() => {
+            getVideoBySearch(name, dispatch);
+          }}
+          className="px-4 py-1 bg-gray-300 m-2 rounded-lg"
+        >
+          {name}
+        </button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -17,9 +17,14 @@ const ButtonList = () => {
     "Statitics",
     "Politics",
     "valentine",
+    "java",
+    "lofi song",
+    "Discipline",
+    "Motivation",
+    "gym",
   ];
   return (
-    <div className="flex w-[90vw] overflow-x-scroll ">
+    <div className="flex w-[95vw] overflow-x-scroll ">
       {btnList.map((list) => (
         <Button name={list} key={list} />
       ))}
